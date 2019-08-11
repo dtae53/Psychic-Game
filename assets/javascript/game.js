0033c5afa954c30cc477313c63a3bcd44de25cdc
Davontae <davontaeglenn@gmail.com>
2019-08-10 18:21:35 -0700

    updated Index and Javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,89 @@
+var lettersGuessed = [];
+var guessesLeft = 10;
+var wins = 0; 
+var losses = 0;
+
+var computerGuess =
+    String.fromCharCode(
+        Math.round(Math.random() * 26) + 97
+    );
+
+console.log(computerGuess);
+document.onkeydown = function(event) {
+    var keyPress = (String.fromCharCode(event.keyCode)).toLowerCase();
+
+    addLetter(keyPress);
+
+}
+
+function addLetter (usersKeypress) {
+
+    var repeatGuess = lettersGuessed.some(function(item){
+        return item === usersKeypress;
+    })
+
+    if (repeatGuess) {
+        alert(usersKeypress + " already guessed. Try again!");
+
+    } else {
+        lettersGuessed.push(usersKeypress);
+        console.log(lettersGuessed);
+        showLettersGuessed();
+        guessMatch(usersKeypress);
+    }
+
+}
+
+function showLettersGuessed() {
+    var tempStr = lettersGuessed.join(", ");
+    document.getElementById("playersGuess").innerHTML = tempStr;
+}
+
+function guessMatch (character) {
+
+    console.log(character);
+    console.log(computerGuess);
+
+    if (character === computerGuess) {
+        wins = wins + 1;
+        showWins();
+        //toggleGame();
+
+    } else if (guessesLeft === 0) {
+        losses = losses + 1; 
+        showLosses();
+        resetVariables ();
+
+    } else {
+        guessesLeft = guessesLeft - 1;
+        showGuessesRemaining();
+    }
+}
+
+//function to show wins
+function showWins() {
+    document.getElementById("numWins").innerHTML = wins;
+}
+
+//function to show guesses remaining
+function showGuessesRemaining() {
+    document.getElementById("numGuesses").innerHTML = guessesLeft;
+} 
+
+function showLosses() {
+    document.getElementById("numLosses").innerHTML = losses;
+}
+
+
+function resetVariables () {
+    lettersGuessed = [];
+    guessesLeft = 10;
+}
+
+function startGame() {
+    showGuessesRemaining();
+    showWins();
+}
+
+
+startGame();
